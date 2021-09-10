439f667bde7755a2646519aebef1d37ad328af7c
Mijalski <kyrcooler@gmail.com>
2021-09-10 23:07:24 +0200

    Block moving

--- src/scenes/game-scene.js
+++ src/scenes/game-scene.js
@@ -22,12 +22,15 @@ export default class GameScene extends Phaser.Scene
               [0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 1, 0, 0, 0],
               [0, 0, 0, 0, 0, 1, 0, 0, 0] ];
+    tileImages = [];
     tileScale; pickerTileScale;
     margin; halfMargin;
     screenSize;
     tileSize; halfTileSize;
-    activeBlocks;
+    activeBlockDefinitions; activeBlocks;
     blockPickerPositionX; blockPickerPositionY;
+    chosenBlockDefinition; chosenBlockImages;
+    chosenGridTile;
 
     constructor ()
     {
@@ -59,50 +62,157 @@ export default class GameScene extends Phaser.Scene
         this.blockPickerPositionX = this.screenSize / 3;
         this.blockPickerPositionY = this.screenSize + (this.margin * 1.5);
 
-        this.activeBlocks = this.getRandomBlocks();
+        this.activeBlockDefinitions = this.getRandomBlocks();
 
-        this.input.on('pointerup', (pointer, objectsClicked) => {    
-            console.log('released');
+        this.input.on('pointerup', (pointer, objectsClicked) => {   
+            this.dropBlock(); 
         });
 
+        for (let i = 0; i < gridSize; i++) {
+            this.tileImages[i] = [];
+        }
         this.renderGrid();
         this.renderBlocks();
     }
 
+    update() {
+        if (this.chosenBlockDefinition) {
+            this.moveBlock(this.chosenBlockImages, this.chosenBlockDefinition, 
+                this.chosenBlockDefinition[0] === 0 
+                    ? this.input.mousePointer.x - this.tileSize
+                    : this.input.mousePointer.x, 
+                this.input.mousePointer.y - this.tileSize);
+        }
+    }
+
+    dropBlock() {
+        this.chosenBlockDefinition.forEach(block => {
+
+        });
+        this.chosenBlockImages = undefined;
+        this.chosenBlockDefinition = undefined;
+    }
+
     renderGrid() {
-        for(let x = 0; x < gridSize; x++) {
-            for(let y = 0; y < gridSize; y++) {
-                this.add.image(
-                            this.margin + (x * this.tileSize) + x + this.halfTileSize, 
-                            this.margin + (y * this.tileSize) + y + this.halfTileSize,
-                            this.tiles[y][x] == 0 ? 'tile1' : 'block')
-                        .setScale(this.tileScale);
+        for(let i = 0; i < gridSize; i++) {
+            for(let j = 0; j < gridSize; j++) {
+                this.tileImages[j].push(this.add.image(
+                                            this.margin + (j * this.tileSize) + j + this.halfTileSize, 
+                                            this.margin + (i * this.tileSize) + i + this.halfTileSize,
+                                            this.tiles[j][i] == 0 ? this.getTileImage(j, i) : 'block')
+                                .setScale(this.tileScale)
+                                .setInteractive()
+                                .on('pointerover', pointer => {
+                                    if (this.chosenBlockDefinition) {
+                                        this.resetGridHighlight();
+                                        if(this.isValidPlacement(this.chosenBlockDefinition, j, i)) {
+                                            this.highlightGrid(j, i);
+                                        }
+                                    }
+                                }));
             }
         }
     }
 
-    renderBlocks() {
-        this.activeBlocks.forEach((blockDefinition, idx) => {
-            let y = this.blockPickerPositionY;
-            let x = this.blockPickerPositionX * idx + this.margin;
-            blockDefinition.forEach(block => {
-                if(block === -1) {
-                    y += this.halfTileSize;
-                    x = this.blockPickerPositionX * idx + this.margin;
-                } 
-                else {
-                    x += this.halfTileSize;
-                    if(block === 1) {
-                        this.add.image(x + this.halfTileSize, y + this.halfTileSize, 'block')
-                                .setScale(this.pickerTileScale)
-                                .setName(`block-${idx}`)
-                                .setInteractive()
-                                .on('pointerdown', pointer => {
-                                    console.log(idx);
-                                });
-                    }
+    resetGridHighlight() {
+        for(let i = 0; i < gridSize; i++) {
+            for(let j = 0; j < gridSize; j++) {
+                this.tileImages[i][j].setTint(0xffffff);
+            }
+        }
+    }
+
+    highlightGrid(j, i) {
+        const startingJ = j;
+        this.chosenBlockDefinition.forEach(block => {
+            if (block === 1) {
+                if (j < gridSize && i < gridSize && this.tiles[j][i] !== 1) {
+                    this.tileImages[j][i].setTint(0x333333);
                 }
-            });
+                else {
+                    return;
+                }
+            } 
+
+            if (block === -1){
+                i++;
+                j = startingJ;
+            } else {
+                j++;
+            }
+        });
+    }
+
+    isValidPlacement(blockDefinition, j, i) {
+        let isValid = true;
+        blockDefinition.forEach(block => {
+            if (block === 1) {
+                if (j >= gridSize || i >= gridSize || this.tiles[j][i] === 1) {
+                    isValid = false;
+                }
+            } 
+        });
+
+        return isValid;
+    }
+
+    getTileImage(x, y) {
+        if ((x > 2 && x < 6 && (y < 3 || y > 5)) 
+            || (y > 2 && y < 6 && (x < 3 || x > 5)))
+            return 'tile1';
+        return 'tile2';
+    }
+
+    renderBlocks() {
+        this.activeBlockDefinitions.forEach((blockDefinition, idx) => {
+            let x = this.blockPickerPositionX * idx + this.margin;
+            let y = this.blockPickerPositionY;
+            this.renderBlock(blockDefinition, x, y, false);
+        });
+    }
+
+    moveBlock(blockImages, blockDefinition, x, y) {
+        let startingX = x;
+        let counter = 0;
+        blockDefinition.forEach(block => {
+            if(block === -1) {
+                y += this.tileSize;
+                x = startingX;
+            } 
+            else {
+                if(block === 1) {
+                    blockImages[counter]
+                        .setPosition(x + this.tileSize, y + this.tileSize);
+                    counter++;
+                }
+                x += this.tileSize;
+            }
+        });
+    }
+
+    renderBlock(blockDefinition, x, y) {
+        let startingX = x;
+        let blockImages = [];
+        blockDefinition.forEach(block => {
+            if(block === -1) {
+                y += this.halfTileSize;
+                x = startingX;
+            } 
+            else {
+                x += this.halfTileSize;
+                if(block === 1) {
+                    blockImages.push(this.add.image(x + this.halfTileSize, y + this.halfTileSize, 'block')
+                                    .setScale(this.pickerTileScale)
+                                    .setInteractive()
+                                    .on('pointerdown', pointer => {
+                                        blockImages.forEach(img => {
+                                            img.setScale(this.tileScale);
+                                        });
+                                        this.chosenBlockImages = blockImages;
+                                        this.chosenBlockDefinition = blockDefinition;
+                                    }));
+                }
+            }
         });
     }
 
